917129af777cf479f1dfc241ff4f28047c4ca5e7
semickolon <mickolenulud@gmail.com>
2020-08-19 21:20:34 +0800

    Animate pause screen

--- PauseScreen.gd
+++ PauseScreen.gd
@@ -1,5 +1,7 @@
 extends PanelContainer
 
+onready var anim_player = $AnimationPlayer
+
 var _paused := false
 
 func _ready():
@@ -8,9 +10,10 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		_paused = not _paused
+		
+		if _paused:
+			anim_player.play("enter")
+		
+		yield(get_tree(), "idle_frame")
 		get_tree().paused = _paused
 		visible = _paused
-		
-		var time_scale = 0 if _paused else 1
-		Engine.time_scale = time_scale
-		VisualServer.set_shader_time_scale(time_scale)
